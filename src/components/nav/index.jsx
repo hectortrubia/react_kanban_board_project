a054f2caecfbe89e80c8da21820b03d924859c2d
hectortrubia <HEALFER@YAHOO.ES>
2023-02-15 14:45:28 +0100

    nav and date changes

--- src/components/nav/index.jsx
+++ src/components/nav/index.jsx
@@ -5,6 +5,7 @@ function Nav() {
 
     return (
         <React.Fragment>
+            
             <div className='nav'>
                 <img className="img" src="https://cdn-icons-png.flaticon.com/512/5065/5065589.png" alt="img" />
                 <p className="text">Kanban Board</p>
